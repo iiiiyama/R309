7aae565049d0bc391ba9ba5c97efc2292540e4ad
e2101633 <bptstantony@gmail.com>
2022-11-08 16:01:45 +0100

    ajout de thread_intro.py

--- thread_intro.py
+++ thread_intro.py
@@ -22,3 +22,11 @@ t1.join()
 t2.join()
 end = time.perf_counter()
 print(f"Tasks ended in {round(end - start, 2)} second(s)")
+
+T = []
+for i in range(100):
+    T.append(threading.Thread(target=task, args=[i]))
+for i in range(len(T)):
+    T[i].start()
+for i in range(len(T)):
+    T[i].join()
