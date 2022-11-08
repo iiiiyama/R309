beb65b0340b6e0b7fcede7f586460043a0575702
e2101633 <bptstantony@gmail.com>
2022-11-08 16:10:12 +0100

    ajout de 100_thread.py et modification de thread_intro.py

---
+++ 100_thread.py
@@ -0,0 +1,19 @@
+import threading
+import time
+
+if  __name__ == '__main__':
+
+    def task(i):
+        print(f"Task {i} starts for {i+1} second(s)")
+        time.sleep(i+1)
+        print(f"Task {i} ends")
+
+    T = []
+    for i in range(100):
+        T.append(threading.Thread(target=task, args=[i]))
+    for i in range(len(T)):
+        T[i].start()
+    for i in range(len(T)):
+        T[i].join()
+
+
--- thread_intro.py
+++ thread_intro.py
@@ -1,32 +1,26 @@
 import threading
 import time
 
-def task(i):
-    print(f"Task {i} starts")
-    time.sleep(1)
-    print(f"Task {i} ends")
-start = time.perf_counter()
-t1 = threading.Thread(target=task, args=[1])
-t1.start()
-t1.join()
+if  __name__ == '__main__':
+    def task(i):
+        print(f"Task {i} starts")
+        time.sleep(1)
+        print(f"Task {i} ends")
+    start = time.perf_counter()
+    t1 = threading.Thread(target=task, args=[1])
+    t1.start()
+    t1.join()
 
-end = time.perf_counter()
-print(f"Tasks ended in {round(end - start, 2)} second(s)")
+    end = time.perf_counter()
+    print(f"Tasks ended in {round(end - start, 2)} second(s)")
 
-start = time.perf_counter()
-t1 = threading.Thread(target=task, args=[1])
-t1.start()
-t2 = threading.Thread(target=task, args=[2])
-t2.start()
-t1.join()
-t2.join()
-end = time.perf_counter()
-print(f"Tasks ended in {round(end - start, 2)} second(s)")
+    start = time.perf_counter()
+    t1 = threading.Thread(target=task, args=[1])
+    t1.start()
+    t2 = threading.Thread(target=task, args=[2])
+    t2.start()
+    t1.join()
+    t2.join()
+    end = time.perf_counter()
+    print(f"Tasks ended in {round(end - start, 2)} second(s)")
 
-T = []
-for i in range(100):
-    T.append(threading.Thread(target=task, args=[i]))
-for i in range(len(T)):
-    T[i].start()
-for i in range(len(T)):
-    T[i].join()
