b22db730f5d51c2b7024f991b334fcab42a0e59f
e2101633 <bptstantony@gmail.com>
2022-11-09 10:32:56 +0100

    ajout du multi_process.py pour le multiprocessing note de serveur synchrone et asynchrone + fabriquer un discord

--- multi_process.py
+++ multi_process.py
@@ -1,15 +1,17 @@
 import time
 import multiprocessing
 
-def task():
-    print(f"Task starts for 1 second")
+def task(i):
+    print(f"Task {i} starts for 1 second")
     time.sleep(1)
-    print(f"Task ends")
+    print(f"Task {i} ends")
 if __name__ == '__main__':
     start = time.perf_counter()
-    p1 = multiprocessing.Process(target=task)
+    p1 = multiprocessing.Process(target=task, args=)
     p2 = multiprocessing.Process(target=task)
     p1.start()
     p2.start()
+    p1.join()
+    p2.join()
     end = time.perf_counter()
     print(f"Tasks ended in {round(end - start, 2)} second(s)")
