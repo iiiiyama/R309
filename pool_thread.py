098c62332d93579f9a554af9dad52cb6b9bc9a90
e2101633 <bptstantony@gmail.com>
2022-11-09 08:57:22 +0100

    modification de pool_thread.py -> il marche maintenant

--- pool_thread.py
+++ pool_thread.py
@@ -3,24 +3,20 @@ import concurrent.futures
 import requests
 import sys
 
+img_urls = [
+        'https://cdn.pixabay.com/photo/2022/10/25/13/28/hanoi-7545860__340.jpg'
+    ]
+
 def download_image(img_url):
     img_bytes = requests.get(img_url).content
-    img_name = img_url.split('/')[4]
+    img_name = img_url.split('/')[len(img_url.split('/'))-1]
     with open(img_name, 'wb') as img_file:
         img_file.write(img_bytes)
         print(f"{img_name} was downloaded")
 
-def main():
-    img_urls = [
-        'https://pixabay.com/get/gf1a238e2982a8ca3133ba4a9f7cb4db8438be90b74885e72d16ec7e3f604d00e150c9b00b39a98a59942255bd9de38983077cfaa161d1b104bb404d273bae119b07fb9fbf160f3e1f8215081f04eaca2_1920.jpg’'
-    ]
+start = time.perf_counter()
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    executor.map(download_image, img_urls)
+end = time.perf_counter()
+print(f"Tasks ended in {round(end - start, 2)} second(s)")
 
-
-    start = time.perf_counter()
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        executor.map(download_image, img_urls)
-    end = time.perf_counter()
-    print(f"Tasks ended in {round(end - start, 2)} second(s)")
-
-if  __name__ == '__main__':
-    sys.exit(main())
